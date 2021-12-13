746f6037b620b52e34b8f55f56f01fd889affdd0
haziqhakimiiz <93898364+haziqhakimiiz@users.noreply.github.com>
2021-12-14 00:28:09 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+  $my_string = "Hello Guys, I am Haziq Hakimi Izhar Bin Harun, the Cloud Computer expert!. Please go to my GitHub accoun https://github.com/haziqhakimiiz "; 
+  echo $my_string;
+?>
